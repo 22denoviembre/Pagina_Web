d9057b3749cc90f025291710d55e2f8a8be584a0
JOSÉ LUIS MUÑOZ PADILLA <USIS043721@ugb.edu.sv>
2021-11-17 16:34:50 -0600

    ingreso

---
+++ WebApplication1/ingreso.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="ingreso.aspx.cs" Inherits="WebApplication1.ingreso" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <center>
+        <h1>Ingreso de Usuarios </h1> 
+    </center>
+ <div>
+     <label>Ingrese su Nombre</label> <br /> <asp:TextBox ID="txtnombre" runat="server"></asp:TextBox>
+     
+ </div>
+    <div> 
+        <label>Ingrese su Usuario</label> <br /> <asp:TextBox ID="txtusuario" runat="server"></asp:TextBox>
+    </div>
+    <div>
+        <label>Ingrese su Contraseña</label> <br /> <asp:TextBox ID="txtclave" runat="server"></asp:TextBox>
+    </div>
+    <br />
+    <asp:Button ID="btbguardar" runat="server" Text="Guardar" Width="89px" OnClick="btbguardar_Click" />
+    
+    <asp:Button ID="btbcancelar" runat="server" Text="Cancelar" Width="89px" OnClick="btbcancelar_Click" />
+     <br />
+     <br />
+    <div>
+        <asp:Button id="btbconsulta" runat="server" Text="Consultar Tabla" OnClick="btbconsulta_Click"/>
+        <br />
+       <br />
+        <asp:GridView ID="consultaview" runat="server">
+        </asp:GridView>
+    </div>
+     </asp:Content>
---
+++ WebApplication1/ingreso.aspx.designer.cs
@@ -0,0 +1,80 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace WebApplication1
+{
+
+
+    public partial class ingreso
+    {
+
+        /// <summary>
+        /// Control txtnombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtnombre;
+
+        /// <summary>
+        /// Control txtusuario.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtusuario;
+
+        /// <summary>
+        /// Control txtclave.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtclave;
+
+        /// <summary>
+        /// Control btbguardar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btbguardar;
+
+        /// <summary>
+        /// Control btbcancelar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btbcancelar;
+
+        /// <summary>
+        /// Control btbconsulta.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btbconsulta;
+
+        /// <summary>
+        /// Control consultaview.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView consultaview;
+    }
+}
